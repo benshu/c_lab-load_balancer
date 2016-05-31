88b31fe0626cb8a69d2b4b0e3f0018f22b56d30c
Hagay Ben-Shushan <hagaybens@gmail.com>
2016-05-31 18:17:47 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7 -tt
+
+
+def main():
+    import sys
+    if len(sys.argv) != 2:
+        print('Usage: %s <Remote load balancer port>' % sys.argv[0])
+
+if __name__ == "__main__":
+    main()
